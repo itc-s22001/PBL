a7c4827679e295087207526b3a5d8b82404681f3
s22001 <s22001@std.it-college.ac.jp>
2023-08-17 21:53:11 +0900

    カレンダー機能と選択した日付をTextViewに表示が出来た。

--- app/src/main/java/jp/ac/it_college/std/s22001/pbl/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s22001/pbl/MainActivity.kt
@@ -1,11 +1,25 @@
 package jp.ac.it_college.std.s22001.pbl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.DialogFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_setting)
+
+        val intentSetting = Intent(
+            this@MainActivity,
+            SettingActivity::class.java
+        )
+        startActivity(intentSetting)
     }
+//    fun onClickEvent(view: View) {
+//        val dialog: DialogFragment = showDialogFragment()
+//        dialog.show(supportFragmentManager, "DialogFragment")
+//    }
 }
