7a644cd4ebf7f3ae1294adf7814f2cff0dc94919
itc-s22029 <s22029@std.it-college.ac.jp>
2023-08-17 18:14:40 +0900

    サウンド選択画面-画面遷移（選択ボタンを押して戻る）

--- app/src/main/java/jp/ac/it_college/std/s22001/pbl/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s22001/pbl/MainActivity.kt
@@ -1,10 +1,55 @@
 package jp.ac.it_college.std.s22001.pbl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import android.view.View
+import jp.ac.it_college.std.s22001.pbl.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("SoundScreen", "Sub onCreate() called.")
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btSound.setOnClickListener(::onNextClick)
+    }
+
+    override fun onStart() {
+        Log.i("SoundScreen", "Sub onStart() called.")
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        Log.i("SoundScreen", "Sub onRestart() called.")
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        Log.i("SoundScreen", "Sub onResume() called.")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i("SoundScreen", "Sub onPause() called.")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("SoundScreen", "Sub onStop() called.")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.i("SoundScreen", "Sub onDestroy() called.")
+        super.onDestroy()
+    }
+
+    private fun onNextClick(view: View) {
+        val intent = Intent(this, SoundScreen::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/jp/ac/it_college/std/s22001/pbl/SoundScreen.kt
+++ app/src/main/java/jp/ac/it_college/std/s22001/pbl/SoundScreen.kt
@@ -2,10 +2,54 @@ package jp.ac.it_college.std.s22001.pbl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import jp.ac.it_college.std.s22001.pbl.databinding.ActivitySoundScreenBinding
 
 class SoundScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySoundScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("SoundScreen", "Sub onCreate() called.")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sound_screen)
+        binding = ActivitySoundScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvLavelInput.setOnClickListener(::onPreviousClick)
+
+    }
+
+    override fun onStart() {
+        Log.i("SoundScreen", "Sub onStart() called.")
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        Log.i("SoundScreen", "Sub onRestart() called.")
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        Log.i("SoundScreen", "Sub onResume() called.")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.i("SoundScreen", "Sub onPause() called.")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.i("SoundScreen", "Sub onStop() called.")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.i("SoundScreen", "Sub onDestroy() called.")
+        super.onDestroy()
+    }
+
+    private fun onPreviousClick(view: View) {
+        finish()
     }
 }
